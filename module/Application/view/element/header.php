84cbc3a45f16dcd62917e80ca20d8d39db7f1559
Michał Nosek <michal.nosek@velisit.com>
2015-08-26 20:51:12 +0200

    Dojo bootstrap

--- module/Application/view/element/header.php
+++ module/Application/view/element/header.php
@@ -1,13 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php echo $title; ?></title>
-
-    <!-- Bootstrap -->
-    <link href="res/css/bootstrap.min.css" rel="stylesheet">
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <title><?php echo $title; ?></title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="res/css/bootstrap.css">
+        </head>
+    
+        <body>        
+        <script>
+            var dojoConfig = {
+                async: 1,
+                packages: [
+                    { name: "bootstrap", location: "/res/js/dojo-bootstrap" }
+                ]
+            };
+        </script>
+        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/dojo/1.10.4/dojo/dojo.js"></script>
